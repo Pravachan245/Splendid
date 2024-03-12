35c6c54832a14a76c1cab9c4b9ff08e605329229
Pravachan_Rebelly <Pravachan245@gmail.com>
2024-03-12 22:29:05 +0000

    Initial commit

---
+++ app/src/main/java/uk/ac/tees/mad/splendid/LoginRegisterActivity.kt
@@ -0,0 +1,11 @@
+package uk.ac.tees.mad.splendid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
---
+++ app/src/main/java/uk/ac/tees/mad/splendid/ShoppingActivity.kt
@@ -0,0 +1,11 @@
+package uk.ac.tees.mad.splendid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
