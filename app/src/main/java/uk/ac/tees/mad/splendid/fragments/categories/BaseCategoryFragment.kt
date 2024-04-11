8a016267e9a62ef8929aaf33621079654d1e4879
Pravachan245 <Pravachan245@gmail.com>
2024-04-11 21:21:58 +0100

    -- Bottom Navigation -- Tab Layout -- Main Category

---
+++ app/src/main/java/uk/ac/tees/mad/splendid/fragments/categories/BaseCategoryFragment.kt
@@ -0,0 +1,8 @@
+package com.example.kelineyt.fragments.categories
+
+import androidx.fragment.app.Fragment
+import uk.ac.tees.mad.splendid.R
+
+open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
+
+}
---
+++ app/src/main/java/uk/ac/tees/mad/splendid/fragments/categories/MainCategoryFragment.kt
@@ -0,0 +1,12 @@
+package com.example.kelineyt.fragments.categories
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import uk.ac.tees.mad.splendid.R
+
+private val TAG = "MainCategoryFragment"
+
+@AndroidEntryPoint
+class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
+
+}
