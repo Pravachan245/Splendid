079c9df74aab4cacd3de275a4c803f167cd50eda
Pravachan245 <Pravachan245@gmail.com>
2024-04-11 20:05:56 +0100

    -- Shopping Activity -- Setup home,cart,search,profile fragment -- navigation between fragments

---
+++ app/src/main/java/uk/ac/tees/mad/splendid/fragments/shopping/HomeFragment.kt
@@ -0,0 +1,7 @@
+package uk.ac.tees.mad.splendid.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import uk.ac.tees.mad.splendid.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+}
