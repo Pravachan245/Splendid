055c993646144995fdcd1202a76fe5e379a3573f
Pravachan245 <Pravachan245@gmail.com>
2024-04-13 17:58:26 +0100

    -- Best Deals Adapter -- Main Category

--- app/src/main/java/uk/ac/tees/mad/splendid/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/uk/ac/tees/mad/splendid/fragments/categories/MainCategoryFragment.kt
@@ -1,12 +1,108 @@
 package com.example.kelineyt.fragments.categories
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import uk.ac.tees.mad.splendid.R
+import uk.ac.tees.mad.splendid.adapters.BestDealsAdapter
+import uk.ac.tees.mad.splendid.adapters.BestProductsAdapter
+import uk.ac.tees.mad.splendid.adapters.SpecialProductsAdapter
+import uk.ac.tees.mad.splendid.databinding.FragmentMainCategoryBinding
+import uk.ac.tees.mad.splendid.util.Resource
+import uk.ac.tees.mad.splendid.viewmodel.MainCategoryViewModel
 
 private val TAG = "MainCategoryFragment"
 
 @AndroidEntryPoint
 class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
+    private lateinit var binding: FragmentMainCategoryBinding
+    private lateinit var specialProductsAdapter: SpecialProductsAdapter
+    private lateinit var bestDealsAdapter: BestDealsAdapter
+    private lateinit var bestProductsAdapter: BestProductsAdapter
+    private val viewModel by viewModels<MainCategoryViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupSpecialProductsRv()
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.specialProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        showLoading()
+                    }
+                    is Resource.Success -> {
+                        specialProductsAdapter.differ.submitList(it.data)
+                        hideLoading()
+                    }
+                    is Resource.Error -> {
+                        hideLoading()
+                        Log.e(TAG, it.message.toString())
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+    }
+
+
+    private fun setupSpecialProductsRv() {
+        specialProductsAdapter = SpecialProductsAdapter()
+        binding.rvSpecialProducts.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = specialProductsAdapter
+        }
+    }
+
+    private fun setupBestDealsRv() {
+        bestDealsAdapter = BestDealsAdapter()
+        binding.rvBestDealsProducts.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = bestDealsAdapter
+        }
+    }
+
+    private fun setupBestProducts() {
+        bestProductsAdapter = BestProductsAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager =
+                GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductsAdapter
+        }
+    }
+
+
+    private fun hideLoading() {
+        binding.mainCategoryProgressbar.visibility = View.GONE
+    }
+
+    private fun showLoading() {
+        binding.mainCategoryProgressbar.visibility = View.VISIBLE
+
+    }
 
 }
